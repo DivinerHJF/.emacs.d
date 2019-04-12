c6077d08e0ccf3f9ba6e43f2cf5e5ec4f9cc5f69
郝建锋 <1246574158@qq.com>
2019-04-12 12:34:57 +0800

    modified

--- init.el
+++ init.el
@@ -1,67 +1,13 @@
-;; Use Melpa As the Source of Plug-ins
-
-(when (>= emacs-major-version 24)
-    (require 'package)
-    (package-initialize)
-    (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-			     ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
-
-(require 'cl)  ;; cl - Common Lisp Extension
-
-;; Add Packages
-(defvar my/packages '(
-		;; --- Auto-completion ---
-		company
-		;; --- Better Editor ---
-		hungry-delete
-		swiper
-		counsel
-		smartparens
-		;; --- Major Mode ---
-		js2-mode
-		;; --- Minor Mode ---
-		nodejs-repl
-		exec-path-from-shell
-		;; --- Themes ---
-		monokai-theme
-		;; solarized-theme
-		) "Default packages")
-
-(setq package-selected-packages my/packages)
-
-(defun my/packages-installed-p ()
-  (loop for pkg in my/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
-
-(unless (my/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg my/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-;; Find Executable Path on OS X
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
 
 ;;--------------------------------------------------------------------------
 
+;; Package Management
+(require 'init-packages)
 
-;;- config for company
-(global-company-mode 1)
 
-;;- config for hungry-delete
-(require 'hungry-delete)
-(global-hungry-delete-mode)
-
-;;- config for swiper and counsel
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
+;; ivy-mode
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
@@ -78,14 +24,6 @@
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-;;- config for smartparens
-(require 'smartparens-config)
-(smartparens-global-mode t)
-
-;;- config for monokai-theme
-(load-theme 'monokai t)
-
 
 ;;-------------------------------------------------------------------------
 
@@ -128,12 +66,14 @@
 (show-paren-mode 1)        ;;- Display parentheses pairing
 (menu-bar-mode -1)         ;;- hide the meun bar
 
+
 ;;-------------------------------------------------------------------------
 
 
 ;; System Variable Settings
 
 (setq make-backup-files nil)               ;;- disable backup file
+(setq auto-save-default nil)               ;;- disable auto save
 (setq inhibit-splash-screen 1)             ;;- turn off splash screen
 (setq initial-frame-alist
       (quote ((fullscreen . maximized))))  ;;- fullscreen
@@ -159,10 +99,12 @@
 (global-set-key "\C-x\ \C-r"' recentf-open-files)
 
 
-
-
-
-
+;;- define your abbrevs
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(
+					    ;; signature
+					    ("8di" "diviner")
+					    ))
 
 
 
